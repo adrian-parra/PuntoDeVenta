97e807ab6b5e7ce611d8e89cb471df2df8ae2228
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 15:06:02 -0700

    Se agrego el input password en form agregar empleado ,para que pueda iniciar en el sistema con su rol.

--- src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
+++ src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-employees-createemployee',
@@ -11,11 +12,12 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
   empleadoFormRegistro = new FormGroup({
     nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
     correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
+    clave: new FormControl('' ,[Validators.minLength(8) ,Validators.required]) ,
     telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
     rol: new FormControl('' ,[Validators.required ]) ,
   });
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -25,7 +27,7 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
   }
 
   irComponenteListaEmpleados(){
-    
+    this.router.navigate(['dashboard/employees/list'])
   }
 
 }
