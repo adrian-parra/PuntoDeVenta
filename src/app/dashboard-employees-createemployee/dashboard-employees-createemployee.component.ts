9420786406d29d7905cc075336c9db91e393d9cf
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 16:01:17 -0700

    Funcionalidad para agregar un nuevo cliente finalizada.

--- src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
+++ src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../servicios/api.service';
 
 @Component({
   selector: 'app-dashboard-employees-createemployee',
@@ -10,19 +11,46 @@ import { Router } from '@angular/router';
 export class DashboardEmployeesCreateemployeeComponent implements OnInit {
   
   empleadoFormRegistro = new FormGroup({
+    id_usuario: new FormControl(null),
     nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
+    estatus:new FormControl('a'),
+    id_nombre_empresa: new FormControl(21),
+    id_rol:new FormControl(),
     correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
     clave: new FormControl('' ,[Validators.minLength(8) ,Validators.required]) ,
-    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
     rol: new FormControl('' ,[Validators.required ]) ,
   });
 
-  constructor(private router:Router) { }
+  constructor(private router:Router ,public apiService:ApiService) { }
 
   ngOnInit(): void {
   }
 
   agregarEmpleado() {
+    
+    let rol = this.empleadoFormRegistro.controls.rol.value
+    let id_rol = this.empleadoFormRegistro.controls.id_rol
+
+    if(rol == "Administrador"){
+      id_rol.setValue(4)
+    }else if(rol == "Gerente"){
+      id_rol.setValue(3)
+    }else if(rol == "Cajero"){
+      id_rol.setValue(2)
+    }
+
+    console.log(this.empleadoFormRegistro.controls)
+  
+    this.apiService.addEmpleado(this.empleadoFormRegistro.value).subscribe((r:any) =>{
+      if(r['0'] == true){
+        this.empleadoFormRegistro.reset
+        alert("Cuenta creada")
+      }else {
+        alert(r['2'])
+      }
+    })
+  
 
   }
 
