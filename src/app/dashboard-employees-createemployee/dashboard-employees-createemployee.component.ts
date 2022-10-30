5eda92d1840034ee388e0a9d6018d2af82b91abb
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 14:05:43 -0700

    Diseño en componente agregar empleado terminado.

--- src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
+++ src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-employees-createemployee',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-employees-createemployee.component.css']
 })
 export class DashboardEmployeesCreateemployeeComponent implements OnInit {
+  
+  empleadoFormRegistro = new FormGroup({
+    nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
+    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10)]) ,
+    rol: new FormControl('' ,[Validators.required ]) ,
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agregarEmpleado() {
+
+  }
+
+  irComponenteListaEmpleados(){
+    
   }
 
 }
