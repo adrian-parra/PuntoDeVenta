f8783b42130d8c6c3d274d1c6165b25a67f58fab
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 11:46:26 -0700

    Se agrego el service login-guard para proteger rutas.

---
+++ src/app/servicios/login-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
+import { Observable } from "rxjs";
+
+export class LoginGuard implements CanActivate {
+
+    
+
+    constructor(private cookie:CookieService , private router:Router){
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if(this.cookie.check("login")){
+            return true;
+        }else {
+            this.router.navigate(['login']);
+            return false;
+        }
+    }
+
+}
