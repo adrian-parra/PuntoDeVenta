daf24c1d943d0c8be96c72c38c29e95f9fd1e04e
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 16:13:40 -0700

    Logica para agregar un nuevo empleado actualizada.

--- src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
+++ src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from '../servicios/api.service';
 
 @Component({
@@ -15,14 +16,14 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
     nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
     telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
     estatus:new FormControl('a'),
-    id_nombre_empresa: new FormControl(21),
+    id_nombre_empresa: new FormControl(),
     id_rol:new FormControl(),
     correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
     clave: new FormControl('' ,[Validators.minLength(8) ,Validators.required]) ,
     rol: new FormControl('' ,[Validators.required ]) ,
   });
 
-  constructor(private router:Router ,public apiService:ApiService) { }
+  constructor(private router:Router ,public apiService:ApiService ,private cookie:CookieService) { }
 
   ngOnInit(): void {
   }
@@ -32,6 +33,8 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
     let rol = this.empleadoFormRegistro.controls.rol.value
     let id_rol = this.empleadoFormRegistro.controls.id_rol
 
+    this.empleadoFormRegistro.controls.id_nombre_empresa.setValue(this.cookie.get('id_nombre_empresa'))
+
     if(rol == "Administrador"){
       id_rol.setValue(4)
     }else if(rol == "Gerente"){
@@ -40,7 +43,6 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
       id_rol.setValue(2)
     }
 
-    console.log(this.empleadoFormRegistro.controls)
   
     this.apiService.addEmpleado(this.empleadoFormRegistro.value).subscribe((r:any) =>{
       if(r['0'] == true){
