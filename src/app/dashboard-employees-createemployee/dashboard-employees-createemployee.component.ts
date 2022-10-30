24406930711cb1241bd831e4e7f9f37da2f02b49
adrian <adrian.adrianborquez@gmail.com>
2022-10-30 14:48:46 -0700

    Validaciones en form agregar empleado terminadas.

--- src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
+++ src/app/dashboard-employees-createemployee/dashboard-employees-createemployee.component.ts
@@ -11,7 +11,7 @@ export class DashboardEmployeesCreateemployeeComponent implements OnInit {
   empleadoFormRegistro = new FormGroup({
     nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
     correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
-    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10)]) ,
+    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
     rol: new FormControl('' ,[Validators.required ]) ,
   });
 
