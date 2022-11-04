c8a8a7ab0ed4e35e83afa544bebf90e23232621e
adrian <adrian.adrianborquez@gmail.com>
2022-11-03 21:13:33 -0700

    Se creo un FormGroup para formulario registro de cliente.

--- src/app/dashboard-clients-createcustomer/dashboard-clients-createcustomer.component.ts
+++ src/app/dashboard-clients-createcustomer/dashboard-clients-createcustomer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-clients-createcustomer',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardClientsCreatecustomerComponent implements OnInit {
 
+  clienteFormRegistro = new FormGroup({
+    nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    correo: new FormControl('' ,[Validators.email ,Validators.required]) ,
+    telefono: new FormControl('' ,[Validators.required ,Validators.minLength(10) ,Validators.maxLength(10) ,Validators.pattern("[0-9]*")]) ,
+    id_nombre_empresa: new FormControl(null),
+    direccion: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    ciudad: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    estado: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    codigo_postal: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
+    nota: new FormControl('' ,[Validators.required ,Validators.minLength(5) ,Validators.maxLength(5) ,Validators.pattern("[0-9]*")]) ,
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addCliente() {
+    
   }
 
 }
