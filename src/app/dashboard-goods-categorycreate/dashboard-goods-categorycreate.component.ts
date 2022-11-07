7596e54dc8a8d30b3e3e71798d75684d05f77a6e
adrian <adrian.adrianborquez@gmail.com>
2022-11-06 22:10:28 -0700

    Codigo para agregar una nueva categoria de articulo terminado.

--- src/app/dashboard-goods-categorycreate/dashboard-goods-categorycreate.component.ts
+++ src/app/dashboard-goods-categorycreate/dashboard-goods-categorycreate.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../servicios/api.service';
 
 @Component({
   selector: 'app-dashboard-goods-categorycreate',
@@ -12,12 +14,23 @@ export class DashboardGoodsCategorycreateComponent implements OnInit {
     nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
  });
 
-  constructor() { }
+  constructor(private apiService:ApiService , private router:Router) { }
 
   ngOnInit(): void {
   }
+
+  irComponneteListaCategoriaArticulo(){
+    this.router.navigate(['dashboard/goods/categories'])
+  }
   
   addCategoriaArticulo(){
-    
+    this.apiService.addCategoriaArticulo(this.categoriaArticuloFormRegistro.value).subscribe((r:any) =>{
+      if(r['0'] == true){
+        alert("Categoria registrada")
+        this.router.navigate(['dashboard/goods/categories'])
+      }else {
+        alert(r['2'])
+      }
+    })
   }
 }
