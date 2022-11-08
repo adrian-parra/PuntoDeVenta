ff048e4cf34c5a827229306346bf78cd6ebedcb1
adrian <adrian.adrianborquez@gmail.com>
2022-11-07 21:22:02 -0700

    Pruebas a modulo para cookies.

--- src/app/dashboard-goods-categorycreate/dashboard-goods-categorycreate.component.ts
+++ src/app/dashboard-goods-categorycreate/dashboard-goods-categorycreate.component.ts
@@ -1,36 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from '../servicios/api.service';
 
 @Component({
   selector: 'app-dashboard-goods-categorycreate',
   templateUrl: './dashboard-goods-categorycreate.component.html',
-  styleUrls: ['./dashboard-goods-categorycreate.component.css']
+  styleUrls: ['./dashboard-goods-categorycreate.component.css'],
 })
 export class DashboardGoodsCategorycreateComponent implements OnInit {
-  
   categoriaArticuloFormRegistro = new FormGroup({
-    nombre: new FormControl('' ,[Validators.required ,Validators.minLength(2)]) ,
- });
+    nombre: new FormControl('', [Validators.required, Validators.minLength(2)]),
+  });
 
-  constructor(private apiService:ApiService , private router:Router) { }
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private cookie: CookieService
+  ) {}
 
   ngOnInit(): void {
+    //this.cookie.delete('login'); // delete Test cookie from the browser
+    //this.cookie.deleteAll(); //Deletes all cookies that can currently be accessed
+    //const allCookies: {} = this.cookie.getAll(); // get all cookies
+    // console.log(allCookies)
+    //console.log(isLogin)
   }
 
-  irComponneteListaCategoriaArticulo(){
-    this.router.navigate(['dashboard/goods/categories'])
+  irComponneteListaCategoriaArticulo() {
+    this.router.navigate(['dashboard/goods/categories']);
   }
-  
-  addCategoriaArticulo(){
-    this.apiService.addCategoriaArticulo(this.categoriaArticuloFormRegistro.value).subscribe((r:any) =>{
-      if(r['0'] == true){
-        alert("Categoria registrada")
-        this.router.navigate(['dashboard/goods/categories'])
-      }else {
-        alert(r['2'])
-      }
-    })
+
+  addCategoriaArticulo() {
+    this.apiService
+      .addCategoriaArticulo(this.categoriaArticuloFormRegistro.value)
+      .subscribe((r: any) => {
+        if (r['0'] == true) {
+          alert('Categoria registrada');
+          this.router.navigate(['dashboard/goods/categories']);
+        } else {
+          alert(r['2']);
+        }
+      });
   }
 }
