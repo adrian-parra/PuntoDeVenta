4f253f58f73b2b7caa7dab88d809bffaf84ef594
adrian <adrian.adrianborquez@gmail.com>
2022-11-11 16:38:24 -0700

    Diseño en componente lista de articulos terminado.

--- src/app/dashboard-goods-price/dashboard-goods-price.component.ts
+++ src/app/dashboard-goods-price/dashboard-goods-price.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardGoodsPriceComponent implements OnInit {
 
+  articulosDisponibles:Boolean = false
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  irAgregarArticulo(){
+
   }
 
 }
