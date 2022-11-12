b142777ad6d14dc1eec5a8d06567b2deadf0d4da
adrian <adrian.adrianborquez@gmail.com>
2022-11-11 17:24:28 -0700

    Se creo modelo para articulo.

---
+++ src/app/modelos/articulo.ts
@@ -0,0 +1,10 @@
+export interface Articulo {
+    id:number ,
+    nombre:string ,
+    precio:number ,
+    coste:number ,
+    nombre_categoria:string,
+    stock:number ,
+    stock_bajo:number ,
+    stock_optimo:number
+}
