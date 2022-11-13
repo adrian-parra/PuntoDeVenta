3f8e555d85ad58408e27a9ada73c27c6fd9fca77
adrian <adrian.adrianborquez@gmail.com>
2022-11-12 22:04:25 -0700

    se agrego validaciones en formulario ,agregar articulo.

--- src/app/dashboard-goods-new/dashboard-goods-new.component.ts
+++ src/app/dashboard-goods-new/dashboard-goods-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-goods-new',
@@ -10,16 +11,35 @@ export class DashboardGoodsNewComponent implements OnInit {
 
   articuloFormRegistro = new FormGroup({
     nombre: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    categoria:new FormControl('',[Validators.required]),
+    descripcion:new FormControl(''),
+    disponible:new FormControl(''),
+    vendido_por:new FormControl('',[Validators.required]),
+    ref:new FormControl(''),
+    precio:new FormControl('',[Validators.required]),
+    coste:new FormControl('',[Validators.required ,Validators.min(0)]),
+    codigo_barras:new FormControl(''),
+    
+    stock:new FormControl('0',[Validators.required]),
+    stock_bajo:new FormControl('' ,[Validators.min(5)]),
+    stock_optimo:new FormControl('' ,[Validators.min(10)]) ,
+    proveedor_principal:new FormControl(''),
+    compra_defecto:new FormControl('')
+
   });
 
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
   addArticulo(){
     
+  }
+
+  irListaArticulos(){
+  this.router.navigate(['dashboard/goods/price'])
   }
 
 }
