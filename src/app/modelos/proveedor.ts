3b626747d1f6c1ed50c96e42375c4e84f16421b9
adrian <adrian.adrianborquez@gmail.com>
2022-11-15 21:43:04 -0700

    Modelo para proveedor creado.

---
+++ src/app/modelos/proveedor.ts
@@ -0,0 +1,14 @@
+export interface Proveedor {
+    id:number,
+    id_empresa:number,
+    nombre:string,
+    correo:string,
+    telefono:string,
+    sitio_web:string,
+    direccion:string,
+    ciudad:string,
+    estado:string,
+    codigo_postal:string,
+    nota:string,
+    estatus:string
+}
