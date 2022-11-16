0bdaf403f6d648bf310eef65fcbd44b5d78c51c2
adrian <adrian.adrianborquez@gmail.com>
2022-11-15 22:06:50 -0700

    Codigo para listar proveedores en componente terminado.

--- src/app/dashboard-inventory-supplierlist/dashboard-inventory-supplierlist.component.ts
+++ src/app/dashboard-inventory-supplierlist/dashboard-inventory-supplierlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Proveedor } from '../modelos/proveedor';
+import { ApiService } from '../servicios/api.service';
 
 @Component({
   selector: 'app-dashboard-inventory-supplierlist',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-inventory-supplierlist.component.css']
 })
 export class DashboardInventorySupplierlistComponent implements OnInit {
-
-  constructor() { }
+  proveedores:Proveedor [] = []
+  proveedoresDisponibles:Boolean = false
+  constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
+    this.getProveedores()
+  }
+  
+  irAgregarProveedor(){
+
   }
 
+  irEditarProveedor(id:number){
+
+  }
+
+  getProveedores(){
+    this.apiService.getProveedores().subscribe(proveedores =>{
+      console.log(proveedores.length)
+
+      this.proveedores = proveedores
+
+      if(this.proveedores.length > 0) this.proveedoresDisponibles = true
+    })
+  }
 }
