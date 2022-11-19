555968e9b0267f1733ffd04a75eb16dbcb7655ac
adrian <adrian.adrianborquez@gmail.com>
2022-11-19 14:25:05 -0700

    Se modifico el modelo de datos historial de invenytario.

--- src/app/modelos/historialInventario.ts
+++ src/app/modelos/historialInventario.ts
@@ -1,4 +1,9 @@
 export interface HistorialInventario {
     id:number,
-    
+    id_articulo:number,
+    id_inventario:number,
+    id_motivo:number,
+    referencia_id:number,
+    ajuste:number,
+    stock_final:number
 }
