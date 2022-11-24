8d78310eedac46536ce4638558dfad4efd66c092
adrian <adrian.adrianborquez@gmail.com>
2022-11-24 13:55:54 -0700

    Modelo para detalleOrdenCompra creado.

---
+++ src/app/modelos/detalleOrdenCompra.ts
@@ -0,0 +1,10 @@
+export interface DetalleOrdenDeCompra {
+    id?:number,
+    id_articulo?:number,
+    articulo?:string ,
+    id_orden_de_compra?:number ,
+    cantidad:number ,
+    costo:number ,
+    total:number ,
+    recibido?:number
+}
