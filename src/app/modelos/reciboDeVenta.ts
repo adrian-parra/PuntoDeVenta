9dfa046a76cecb16ba299e25d63fcecdfffffa9e
adrian <adrian.adrianborquez@gmail.com>
2022-11-26 19:01:20 -0700

    Se creo modelo para recibo de venta.

---
+++ src/app/modelos/reciboDeVenta.ts
@@ -0,0 +1,12 @@
+export interface Recibo {
+    id:number,
+    total:number,
+    cambio:number,
+    fecha:string ,
+    tipo_venta:string,
+    empleado:string ,
+    cliente:any,
+    tipo_pago:string,
+    empresa:string
+
+}
