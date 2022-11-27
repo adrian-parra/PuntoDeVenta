0f0118726330a115b70dd39947dcb4239f8e9857
adrian <adrian.adrianborquez@gmail.com>
2022-11-26 19:00:56 -0700

    Se creo modelo para venta.

---
+++ src/app/modelos/venta.ts
@@ -0,0 +1,9 @@
+export interface Venta {
+    id_empleado:number ,
+    id_cliente:any,
+    id_tipo_pago:number,
+    tipo_venta:string,
+    total:number,
+    cambio:number ,
+    id_empresa:number,
+}
