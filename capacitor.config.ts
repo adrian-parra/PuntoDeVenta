9e8f212f21bea7205b15a9ac63013e5e6bd090af
adrian <adrian.adrianborquez@gmail.com>
2022-11-27 21:14:17 -0700

    Diferentes acciones para subir proyecto a online.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.puntoventa.app',
+  appName: 'punto_de_venta',
+  webDir: 'dist/punto_de_venta',
+  bundledWebRuntime: false
+};
+
+export default config;
